9d15b16971c3c1c3a064245398354da92a033f3f
Tarun Agarwal <101918394+Agarwal7Tarun@users.noreply.github.com>
2022-11-19 17:59:30 +0545

    Add files via upload

--- ques5.py
+++ ques5.py
@@ -1,14 +1,14 @@
-temp = input("Input the  temperature you like to convert? (e.g., 45F, 102C etc.) : ")
-degree = int(temp[:-1])
-i_convention = temp[-1]
-def convert(degree, i_convention):
-    if i_convention.upper() == "C":
-        result = int(round((9 * degree) / 5 + 32))
-        convention = "Fahrenheit"
-    elif i_convention.upper() == "F":
-        result = int(round((degree - 32) * 5 / 9))
-        convention = "Celsius"
-    else:
-        print("Input proper convention.")
+def c_to_f(temp):
+    temp = float(temp)
+    f = (temp * 1.8) + 32
+    result = (f"{temp}C is equivalent to {f}F")
+    return result
 
-convert(degree, i_convention)
+def f_to_c(temp):
+    temp = float(temp)
+    c = (temp - 32) / 1.8
+    result = (f"{temp}F is equivalent to {c}C")
+    return result
+
+print(c_to_f(30))
+print(f_to_c(86))
